b0e4391110de422ae12939e89562726bc25a2366
LexHOK <grizzly.hok@gmail.com>
2019-02-26 15:11:40 +0100

    Error Handling & Alerts

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,9 +1,40 @@
 import React, { Component, Fragment } from 'react';
 import { withAlert } from 'react-alert';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 class Alerts extends Component {
-    componentDidMount() {
-        this.props.alert.show("It Works");
+    static propTypes = {
+        error: PropTypes.object.isRequired,
+        message: PropTypes.object.isRequired
+    }
+
+    componentDidUpdate(prevProps) {
+        const { error, alert, message } = this.props
+        if (error !== prevProps.error) {
+            if (error.msg.name) {
+                alert.error('Name is required');
+            };
+            if (error.msg.email == "This field may not be blank.") {
+                alert.error('Email is required');
+            };
+            if (error.msg.email == "lead with this email already exists.") {
+                alert.error('This email already has a lead');
+            };
+            if (error.msg.message) {
+                alert.error(`Message: ${error.msg.message.join()}`);
+            }
+        }
+
+        if (message !== prevProps.message) {
+            if (message.deleteLead) {
+                alert.success(message.deleteLead);
+            }
+            if (message.addLead) {
+                alert.success(message.addLead);
+            }
+        }
     }
 
     render() {
@@ -11,4 +42,9 @@ class Alerts extends Component {
     }
 }
 
-export default withAlert(Alerts);
+const mapStateToProps = state => ({
+    error: state.errorsReducer,
+    message: state.messagesReducer
+})
+
+export default connect(mapStateToProps)(withAlert(Alerts));
